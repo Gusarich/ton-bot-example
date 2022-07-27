54429fa0c2b8c01b809d16f32a2a192583814f7f
Gusarich <danielsedovzzz@gmail.com>
2022-07-27 14:46:15 +0300

    db.py module and comments in all files

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,4 @@
-from config import *
+# Logging module
 import logging
 
 # Aiogram imports
@@ -8,21 +8,36 @@ from aiogram.types import ParseMode, ReplyKeyboardMarkup, KeyboardButton, \
                           InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils import executor
 
+# Local modules to work with Database and Ton network
+import config
+import ton
+import db
 
+
+# Now all the info about bot work will be printed out to console
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=BOT_TOKEN)
+# Initialize the bot and dispatcher
+bot = Bot(token=config.BOT_TOKEN)
 dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=['start', 'help'])
 async def welcome_handler(message: types.Message):
+    # Function that sends the welcome message with main keyboard to user
+
+    uid = message.from_user.id  # Not neccessary, just to make code shorter
+
+    # If user doesn't exist in database, insert it
+    if not db.check_user(uid):
+        db.add_user(uid)
+
     # Keyboard with two main buttons: Deposit and Balance
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.row(KeyboardButton('Deposit'))
     keyboard.row(KeyboardButton('Balance'))
 
-    # Welcome text
+    # Send welcome text and include the keyboard
     await message.answer('Hi!\nI am example bot '
                          'made for [this article](https://telegra.ph/).\n'
                          'My goal is to show how simple it is to receive '
@@ -35,22 +50,36 @@ async def welcome_handler(message: types.Message):
 @dp.message_handler(commands='balance')
 @dp.message_handler(Text(equals='balance', ignore_case=True))
 async def balance_handler(message: types.Message):
-    await message.answer('Your balance: 0.00 TON')
+    # Function that shows user his current balance
+
+    uid = message.from_user.id
+
+    # Get user balance from database
+    # Also don't forget that 1 TON = 1e9 (billion) NanoTON
+    user_balance = db.get_balance(uid) / 1e9
+
+    # Format balance and send to user
+    await message.answer(f'Your balance: *{user_balance:.2f} TON*',
+                         parse_mode=ParseMode.MARKDOWN)
 
 
 @dp.message_handler(commands='deposit')
 @dp.message_handler(Text(equals='deposit', ignore_case=True))
 async def deposit_handler(message: types.Message):
+    # Function that gives user the address to deposit
+
     uid = message.from_user.id
 
+    # Keyboard with deposit URL
     keyboard = InlineKeyboardMarkup()
     button = InlineKeyboardButton('Deposit',
-                                  url=f'ton://transfer/{DEPOSIT_ADDRESS}&text={uid}')
+                                  url=f'ton://transfer/{config.DEPOSIT_ADDRESS}&text={uid}')
     keyboard.add(button)
 
+    # Send text that explains how to make a deposit into bot to user
     await message.answer('It is very easy to top up your balance here.\n'
                          'Simply send any amount of TON to this address:\n\n'
-                         f'`{DEPOSIT_ADDRESS}`\n\n'
+                         f'`{config.DEPOSIT_ADDRESS}`\n\n'
                          f'And include the following comment: `{uid}`\n\n'
                          'You can also deposit by clicking the button below.',
                          reply_markup=keyboard,
@@ -58,4 +87,5 @@ async def deposit_handler(message: types.Message):
 
 
 if __name__ == '__main__':
+    # Launch the bot
     executor.start_polling(dp, skip_updates=True)
---
+++ src/db.py
@@ -0,0 +1,47 @@
+# We will use the SQLite database
+import sqlite3
+
+
+# Initialize database connection and cursor
+con = sqlite3.connect(':memory:')
+cur = con.cursor()
+
+# Create table "Users" with uid and balance rows
+cur.execute('''CREATE TABLE IF NOT EXISTS Users (
+                uid INTEGER,
+                balance INTEGER
+            )''')
+con.commit()
+
+
+def check_user(uid):
+    # Function to check if user is in database or not
+
+    cur.execute(f'SELECT * FROM Users WHERE uid = {uid}')
+    user = cur.fetchone()
+    if user:
+        return True
+    return False
+
+
+def add_user(uid):
+    # Function to insert new user into database
+
+    # User starts with 0 balance
+    cur.execute(f'INSERT INTO Users VALUES ({uid}, 0)')
+    con.commit()
+
+
+def get_balance(uid):
+    # Function to get user balance
+
+    cur.execute(f'SELECT balance FROM Users WHERE uid = {uid}')
+    balance = cur.fetchone()[0]
+    return balance
+
+
+def add_balance(uid, amount):
+    # Function to increase user balance
+
+    cur.execute(f'UPDATE Users SET balance = balance + {amount} WHERE uid = {uid}')
+    con.commit()
