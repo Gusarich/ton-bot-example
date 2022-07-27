f5523b65f698a3b7c186154d6fff7a8bcb6a514c
Gusarich <danielsedovzzz@gmail.com>
2022-07-27 14:17:30 +0300

    some text, /deposit and /balance commands (not fully implemented yet)

--- src/bot.py
+++ src/bot.py
@@ -1,27 +1,55 @@
-from aiogram import Bot, Dispatcher, types
+from config import *
 import logging
-import config
+
+# Aiogram imports
+from aiogram import Bot, Dispatcher, types
+from aiogram.dispatcher.filters import Text
+from aiogram.types import ParseMode, ReplyKeyboardMarkup, KeyboardButton, \
+                          InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils import executor
 
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=config.BOT_TOKEN)
-dp = Dispatcher()
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
 
 
-@dp.message(commands=['start', 'help'])
-async def handle_welcome(message: types.Message):
-    await message.reply('''Hi!
-I'm example bot made for this article.
-My goal is to show how simple it is to receive payments in TonCoin with Python.
+@dp.message_handler(commands=['start', 'help'])
+async def welcome_handler(message: types.Message):
+    # Welcome text
+    await message.answer('Hi!\nI am example bot made for this article.\n'
+                         'My goal is to show how simple it is to receive '
+                         'payments in TonCoin with Python.\n\n'
+                         'Use keyboard to test my functionality.')
 
-Use keyboard to test my functionality.''')
+    # Send keyboard to user
 
 
-@dp.message()
-async def handle_balance(message: types.Message):
+@dp.message_handler(commands='balance')
+@dp.message_handler(Text(equals='balance', ignore_case=True))
+async def balance_handler(message: types.Message):
     await message.answer('Your balance: 0.00 TON')
 
 
+@dp.message_handler(commands='deposit')
+@dp.message_handler(Text(equals='deposit', ignore_case=True))
+async def deposit_handler(message: types.Message):
+    uid = message.from_user.id
+
+    keyboard = InlineKeyboardMarkup()
+    button = InlineKeyboardButton('Deposit',
+                                  url=f'ton://transfer/{DEPOSIT_ADDRESS}&text={uid}')
+    keyboard.add(button)
+
+    await message.answer('It is very easy to top up your balance here.\n'
+                         'Simply send any amount of TON to this address:\n\n'
+                         f'`{DEPOSIT_ADDRESS}`\n\n'
+                         f'And include the following comment: `{uid}`\n\n'
+                         'You can also deposit by clicking the button below.',
+                         reply_markup=keyboard,
+                         parse_mode=ParseMode.MARKDOWN)
+
+
 if __name__ == '__main__':
-    dp.run_polling(bot)
+    executor.start_polling(dp, skip_updates=True)
